3814aed7ce5bcc5f3567f02e8c1c22702703cc25
do-bright <godqhrgotjs@gmail.com>
2022-04-04 13:35:25 +0900

    별찍기 연습

---
+++ src/j09_반복/Star.java
@@ -0,0 +1,97 @@
+package j09_반복;
+
+import java.util.Scanner;
+
+public class Star {
+
+    public static void main(String[] args) {
+		
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("숫자 입력 : ");
+        int num = scanner.nextInt();
+
+        // *
+        // **
+        // ***
+        // ****
+        // *****
+
+        for(int i=0; i<num; i++){
+            for(int j=0; j<i+1; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        //     *
+        //    **
+        //   ***
+        //  ****
+        // *****
+
+        for(int i=0; i<num; i++){
+
+            for(int j=0; j<num-1-i; j++){
+                System.out.print(" ");
+            }
+            for(int j=0; j<i+1; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        // *****
+        // ****
+        // ***
+        // **
+        // *
+
+        for(int i=0; i<num; i++){
+            for(int j=0; j<num-i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        // *****
+        //  ****
+        //   ***
+        //    **
+        //     *
+
+        for(int i=0; i<num; i++){
+            for(int j=0; j<i; j++){
+                System.out.print(" ");
+            }
+            for(int j=0; j<num-i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        //     *
+        //    ***
+        //   *****
+        //  *******
+        // *********
+        
+        for(int i=0; i<num; i++){
+            for(int j=0; j<num-1-i; j++){
+                System.out.print(" ");
+            }
+            for(int j=0; j<2*i+1; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+	}
+    
+}
